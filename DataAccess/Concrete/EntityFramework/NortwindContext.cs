b18d6f252aa903fe86f567c4e1e5f14c802c81f6
Caglan Atay <caglanatay@windowslive.com>
2021-02-06 23:57:28 +0300

    Core katmanı oluşturuldu

--- DataAccess/Concrete/EntityFramework/NortwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NortwindContext.cs
@@ -7,7 +7,7 @@ using System.Text;
 namespace DataAccess.Concrete.EntityFramework
 {
     // Context: Db tablolaı ile proje classlarını bağlar
-    public class NortwindContext : DbContext
+    public class NorthwindContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -17,6 +17,7 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
     }
 }
